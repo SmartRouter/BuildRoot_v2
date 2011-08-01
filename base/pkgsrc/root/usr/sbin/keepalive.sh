25c6edcd2d6993012b4152c4f964943b3b58fad2
marcosdovale <marcos@dovale.inf.br>
2011-07-31 23:45:32 -0300

    Bug fix Busybox (+ping) and Bug fix keepalive

--- base/pkgsrc/root/usr/sbin/keepalive.sh
+++ base/pkgsrc/root/usr/sbin/keepalive.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Revision 27/07/2009 AdslWiFi añadiendo pruebas a 4 Ping's diferentes
 # LS=0 - Link UP: Internet Conection is OK
 # LS=1 - Link DOWN: IP Conection to the Gateway OK
 # LS=2 - Link WAIT: Link Conection to the Gateway OK
@@ -7,27 +6,18 @@
 # LS=4 - Link DESABLE
 
 . /etc/coyote/coyote.conf
-[ -z "$PING_IP" ] && PING_IP=192.58.128.30
+[ -z "$PING_IP" ] && PING_IP=74.125.229.115
 [ -z "$PING_RETRY" ] && PING_RETRY=3
-# Ping a www.google.com
-[ -z "$PING_IP_2" ] && PING_IP_2=209.85.227.147
-[ -z "$PING_RETRY_2" ] && PING_RETRY_2=3
-# Ping a www.no-ip.com
-[ -z "$PING_IP_3" ] && PING_IP_3=204.16.252.112
-[ -z "$PING_RETRY_3" ] && PING_RETRY_3=3
-# Ping a www.dyndns.com
-[ -z "$PING_IP_4" ] && PING_IP_4=204.13.248.107
-[ -z "$PING_RETRY_4" ] && PING_RETRY_4=3
 TIME=50
 
-PING="/bin/ping -U -Lq -Q 0x10 -c"
-ARPING="/usr/bin/arping -c 1 -I"
+PING="/bin/ping -q -c"
+ARPING="/usr/bin/arping -q -c 1 -I"
 ip="/usr/sbin/ip"
 
 #==========
 net_check() {
-   [ "$INETTYPE" = "ETHERNET_DHCP" ] && return 0
  #1=IP 2=GTW 3=MSK
+ [ "$INETTYPE" = "ETHERNET_DHCP" ] && return 0
  eval `ipcalc -b -n $1 $3`
  IP_NET=$NETWORK
  IP_BRD=$BRODCAST
@@ -40,8 +30,8 @@ net_check() {
 #==========
 arp_check() {
  # if Link Status is FAIL then use arping to discovery new MAC
- loss=`$ARPING ${1} ${2} | grep "Received" | cut -d" " -f2`
- [ "$loss" = "0" ] && LS="3" || LS="2"
+ $ARPING ${1} ${2}
+ [ $? = 0 ] && LS="1" || LS="2"
  return $LS
 }
 
@@ -59,50 +49,13 @@ ip_check() {
  # fast test: if response exit with UP status
  # if no response retry ping with loss value
  # if loss value no exist or egual 100% exit with DOWN status
- 
- # Voy cambiando el valor de PING_IP porque si hago IF's anidados no funciona
- # Primero hago un ping de 1 línea para saber el estado con la IP almacenada
- 
- #echo "PING_IP_TEMP=$PING_IP_TEMP LS=$LS" >> /tmp/ping.txt
- $PING 1 -I ${1} ${PING_IP_TEMP} > /dev/null
- if [ "$LS" = "1" ]; then
-    case $PING_IP_TEMP in
-        $PING_IP)
-            PING_IP=$PING_IP_2
-            PING_RETRY=$PING_RETRY_2
-            ;;
-        $PING_IP_2)
-            PING_IP=$PING_IP_3
-            PING_RETRY=$PING_RETRY_3
-            ;;
-        $PING_IP_3)
-            PING_IP=$PING_IP_4
-            PING_RETRY=$PING_RETRY_4
-            ;;
-        $PING_IP_4)
-            PING_IP=$PING_IP
-            PING_RETRY=$PING_RETRY
-            ;;
-    esac
-    PING_IP_TEMP=$PING_IP
- fi
- 
- #Recupero valor ultimo utilizado por si no pasa por el case anterior
- PING_IP=$PING_IP_TEMP
- 
  $PING 1 -I ${1} ${PING_IP} > /dev/null
  if [ $? = 0 ]; then
-    LS="0"
-    # Línea correcta, responde a ping.
+	LS="0"
  else
-    $PING $PING_RETRY -I ${1} ${PING_IP} >> /dev/null
-    [ $? = 0 ] && LS="0" || LS="1"
+	$PING $PING_RETRY -I ${1} ${PING_IP} > /dev/null
+	[ $? = 0 ] && LS="0" || LS="1"
  fi
-   
- # errors=`$PING $PING_RETRY -I ${1} ${PING_IP} | grep errors | cut -d" " -f6`
- # [ -z "$errors" -o "$errors" != "+3" ] && LS="0" || LS="1"
- 
- # Retornamos el valor de LS
  return $LS
 }
 
